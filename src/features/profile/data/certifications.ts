376ffacf6e8972d8b98a32b4279250576abe9a29
Bienvenu733 <moisebienvenu733@gmail.com>
2026-05-04 17:05:48 +0000

    descriptions dans formation

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -9,7 +9,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Participation au Hackathon d'IA HydThon 2026, axé sur l'innovation dans les solutions d'intelligence artificielle pour les défis contemporains.",
+      "- Compréhension des Fondamentaux ;\n - Manipulation de données avec (Pandas, NumPy, Matplotlib, Seaborn) ;\n - Modélisation avec Scikit-learn (régression, classification, clustering) ;\n - Utilisation d'Algorithmes de Base ( Régression Linéaire, Régression Logistique, K-Nearest Neighbors, Arbres de Décision) ;\n - Évaluation de Modèles (Métriques de Performance, Validation Croisée, Surapprentissage et sous-apprentissage) ;\n - Pratique avec des Bibliothèques Python (Scikit-learn) ;\n - Lancement d'un Mini-Projet (Application de prédiction de pluie).",
   },
   {
     title: "Développement mobile Niveau intermédiaire",
@@ -19,7 +19,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Approfondissement des connaissances Python avec les structures de données avancées, la programmation orientée objet et les modules standards.",
+      "- Comprendre la structure fondamentale d'un programme Flutter, y compris la hiérarchie des widgets et la fonction main ;\n- Comprendre la différence entre les widgets avec état (Stateful Widgets) et les widgets sans état (Stateless Widgets) ;\n - Connaître le langage de programmation Dart et savoir le manipuler afin de développer des programmes avec des structures complexes de ce langage ;\n - Concevoir des interfaces utilisateur élémentaires et simples en utilisant des widgets de mise en forme ;\n - Intégrer une base de données locale en utilisant la bibliothèque SQFLite. Comprendre les concepts de création et de mise à jour des données ;\n- Connaitre le langage de programmation Dart et savoir le manipuler afin de développer des programmes avec des structures complexes de ce langage ;\n- Concevoir des interfaces utilisateur élémentaires et simples en utilisant des widgets de mise en forme ;\n- Intégrer une base de données locale en utilisant la bibliothèque SQFLite. Comprendre les concepts de création et de mise à jour des données.",
   },
   {
     title: "Formation sur la conception de site web avec WordPress",
@@ -29,7 +29,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Maîtrise des concepts avancés de JavaScript incluant la manipulation DOM, les événements, et les meilleures pratiques de programmation.",
+      "- Comprendre les bases de Wordpress et son fonctionnement ;\n- Configuration de boutiques WooCommerce (produits, paniers, paiements) ;\n- Gestion des stocks et commandes en ligne ;\n- Intégration de méthodes de paiement sécurisées ;\n- Utilisation de page builders (Elementor, Gutenberg) pour des designs créatifs.",
   },
   {
     title: "Formation sur l'IA pour booster les performances",
@@ -39,7 +39,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Introduction aux fondamentaux de Python incluant les types de données, les structures de contrôle, les fonctions et la gestion des fichiers.",
+      "- Comprendre les bases et son impact dans divers secteurs ;\n- Explorer des outils comme ChatGPT et MidJourney ;\n- Booster la productivité et la créativité en utilisant l'IA pour automatiser des tâches ou créer du contenu ;\n- Des ateliers pratiques d'apprentissage pour maîtriser les prompts et exploiter le potentiel des IA modernes.",
   },
   {
     title: "Formation en développement Web avec Django",
@@ -49,7 +49,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Participation au hackathon Coding Night 2025 avec développement d'une solution innovative en équipe dans un temps limité.",
+      "- Poser les bases d'une application Django en utilisant des modèles, des vues et des gabarits ;\n- Gérer des données stockées dans une base de données à l'aide du site d'administration de Django ;\n- Construire une interface CRUD dans Django.",
   },
   {
     title: "Formation en développement Web avec React",
@@ -59,7 +59,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Participation au hackathon Coding Night 2025 avec développement d'une solution innovative en équipe dans un temps limité.",
+      "- Comprendre le fonctionnement de React ;\n- Créer une application React complète ;\n- Stocker et récupérer des données avec le state et les effets.",
   },
   {
     title: "Développement mobile Niveau Débutant",
@@ -69,7 +69,7 @@ export const CERTIFICATIONS: Certification[] = [
     credentialID: "",
     credentialURL: "",
     description:
-      "Approfondissement des connaissances Python avec les structures de données avancées, la programmation orientée objet et les modules standards.",
+      "- Compréhension approfondie des approches de développement mobile et des principaux langages utilisés dans le développement d'applications mobiles ;\n- Comprendre les bonnes pratiques de design en matière d'écoconception pour les applications mobiles et être en mesure de concevoir des interfaces utilisateur attrayantes ;\n- Se familiariser avec les règles de bonnes pratiques de création d'interface utilisateur et d'expérience utilisateur (UI/UX) mobile et apprendre à créer des maquettes (wireframes) et des prototypes d'applications mobiles ;\n- Explorer les directives Material App pour concevoir des applications mobiles modernes et intuitives ;\n- Découvrir et utiliser des outils de développement mobile avec zéro code, tels que App Inventor, Flutter Flow et Nowa, pour créer des applications sans nécessiter de connaissances approfondies en programmation.",
     theme: true,
   },
 ];
